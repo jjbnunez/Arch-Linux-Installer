a00f4fa5633c864e2fad53d058a130d31a841e45
Jorge Brandon Nunez <jjbnunez97@gmail.com>
2022-09-16 22:06:32 -0400

    Comment up the first sysinstall script

--- install_sys.sh
+++ install_sys.sh
@@ -1,60 +1,76 @@
 #!/bin/bash
 
-# Never run pacman -Sy on your system!
+
+
+# REMINDER: Never run pacman -Sy on your system!
 pacman -Sy dialog --noconfirm
 
-timedatctl set-ntp true
+# Enable automatic synchronization of the system clock with NTP. 
+timedatectl set-ntp true
 
-dialog --defaultno --title "Are you sure?" --yesno \
-"This is my personal arch linux install. \n\n\
-It will DESTROY EVERYTHING on one of your hard disks. \n\n\
-Don't say YES if you are not sure what you're doing! \n\n\
+# Warn the user that this script will wipe a hard drive.
+dialog --defaultno --title "WARNING" --yesno \
+"Hi! My name is JJ Nunez. \n\n\
+This is my personal Arch Linux installer script. \n\n\
+This script will DESTROY EVERYTHING on one of your drives. \n\n\
+If you're not sure what you're doing, please select NO. \n\n\
+Otherwise, proceed with the installation! \n\n\
 Do you want to continue?" 15 60 || exit
 
-dialog --no-cancel --inputbox "Enter a name for your computer." \
+# Prompt the user for a hostname.
+dialog --no-cancel --inputbox "Enter a hostname for your computer:" \
     10 60 2> comp
 
+# Read the hostname in as a variable.
 comp=$(cat comp) && rm comp
 
-# Verify boot (UEFI or BIOS)
+# Verify boot type (UEFI or BIOS).
 uefi=0
 ls /sys/firmware/efi/efivars 2> /dev/null && uefi=1
 
-# Choosing the hard disk
+# Collect a list of storage devices.
 devices_list=($(lsblk -d | awk '{print "/dev/" $1 " " $4 " on"}' \
     | grep -E 'sd|hd|vd|nvme|mmcblk'))
 
-dialog --title "Choose your hard drive" --no-cancel --radiolist \
-"Where do you want to install your new system? \n\n\
-Select with SPACE, valid with ENTER. \n\n\
-WARNING: Everything will be DESTROYED on the hard disk!" \
+# Prompt the user to select one of the storage devices.
+dialog --title "Choose your drive" --no-cancel --radiolist \
+"Where do you want to install the system? \n\n\
+Select a device with SPACE, and confirm your selection with ENTER. \n\n\
+WARNING: Everything will be DESTROYED on the drive you select!" \
 15 60 4 "${devices_list[@]}" 2> hd
 
+# Read the drive in as a variable.
 hd=$(cat hd) && rm hd
 
+# Prompt the user to input a swap partition size in GiB.
 default_size="8"
 dialog --no-cancel --inputbox \
 "You need three partitions: Boot, Root, and Swap \n\
 The boot partition will be 512M \n\
-The root partition will be the remaining of the hard disk \n\n\
-Enter below the partition size (in Gb) for the Swap. \n\n\
+The root partition will take up most of the remaining space on your drive \n\n\
+Enter below the desired partition size (in Gb) for the swap partition. \n\n\
 If you don't enter anything, it will default to ${default_size}G. \n" \
 20 60 2> swap_size
 
+# Read the swap partition size in as a variable.
 size=$(cat swap_size) && rm swap_size
 
+# If the user did not enter a swap partition size, use the default.
 [[ $size =~ ^[0-9]+$ ]] || size=$default_size
 
+# Prompt the user to wipe the drive
 dialog --no-cancel \
 --title "!!! DELETE EVERYTHING !!!" \
---menu "Choose the way you'll wipe your hard disk ($hd)" \
+--menu "Choose the way you'll wipe your disk ($hd)" \
 15 60 4 \
 1 "Use dd (basic wipe)" \
 2 "Use shred (slow & secure)" \
-3 "No need - my hard disk is empty" 2> eraser
+3 "No need - my drive is empty" 2> eraser
 
+# Read the drive eraser method in as a variable.
 hderaser=$(cat eraser); rm eraser
 
+# Define a function to erase (or skip erasing) a given drive.
 function eraseDisk() {
     case $1 in
         1) dd if=/dev/zero of="$hd" status=progress 2>&1 \
@@ -69,8 +85,10 @@ function eraseDisk() {
     esac
 }
 
+# DANGEROUS: erase the disk.
 eraseDisk "$hderaser"
 
+# Determine boot partition type before running fdisk.
 boot_partition_type=1
 [[ "$uefi" == 0 ]]  && boot_partition_type=4
 
